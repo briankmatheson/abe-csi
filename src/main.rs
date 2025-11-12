89bda51dfc31f718125379835d83586f9fc91749
Brian Matheson <bmath@brianmatheson.com>
2025-11-11 22:40:34 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::net::SocketAddr;
+use std::path::Path;
+use tokio::net::UnixListener;
+use tokio_stream::wrappers::UnixListenerStream;
 use tonic::transport::Server;
 
 mod csi {
@@ -15,14 +17,20 @@ use node::NodeService;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let addr: SocketAddr = "0.0.0.0:50051".parse()?;
-    tracing::info!(%addr, "starting abe-csi-rs server");
+
+    let socket_path = "/csi/csi.sock";
+    if Path::new(socket_path).exists() {
+        std::fs::remove_file(socket_path)?;
+    }
+
+    let uds = UnixListener::bind(socket_path)?;
+    tracing::info!("starting abe-csi-rs on {}", socket_path);
 
     Server::builder()
         .add_service(csi::identity_server::IdentityServer::new(IdentityService::default()))
         .add_service(csi::controller_server::ControllerServer::new(ControllerService::default()))
         .add_service(csi::node_server::NodeServer::new(NodeService::default()))
-        .serve(addr)
+        .serve_with_incoming(UnixListenerStream::new(uds))
         .await?;
 
     Ok(())
